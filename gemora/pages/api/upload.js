f2fef5a21494c3864800155dc2ed4c1b1e390564
Bella2021-crypto <82710380+Bella2021-crypto@users.noreply.github.com>
2025-08-22 14:21:54 +0100

    upload.js

--- gemora/pages/api/upload.js
+++ gemora/pages/api/upload.js
@@ -1,5 +1,5 @@
 // pages/api/upload.js
-import cloudinary from "@/lib/cloudinary";
+import cloudinary from "../lib/cloudinary";
 
 export default async function handler(req, res) {
   if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
